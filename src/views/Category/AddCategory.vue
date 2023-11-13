273282b5778410746557bd050fdc0842386c0b4b
Fificode <lamidifirdaws@gmail.com>
2023-11-14 00:00:53 +0100

    installed axios

--- src/views/Category/AddCategory.vue
+++ src/views/Category/AddCategory.vue
@@ -1,28 +1,46 @@
 <template>
 <div class="mx-5">
     <h3 class="text-center">Add Category</h3>
-<form>
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+        <form>
   <div class="mb-3">
     <label for="InputName" class="form-label">Category Name</label>
-    <input type="text" class="form-control" id="InputName">
+    <input type="text" class="form-control" id="InputName" v-model="categoryName">
   </div>
   <div class="mb-3">
     <label for="InputDescription" class="form-label">Category Description</label>
-    <input type="text" class="form-control" id="InputDescription">
+    <textarea type="text" class="form-control" id="InputDescription" v-model="categoryDescription"></textarea>
   </div>
   <div class="mb-3">
     <label for="InputImage" class="form-label">Category Image</label>
-    <input type="text" class="form-control" id="InputImage">
+    <input type="text" class="form-control" id="InputImage" v-model="imageUrl">
   </div>
   
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="button" class="btn btn-primary" @click="addCategory">Submit</button>
 </form>
+      </div>
+      <div class="col-3"></div>
+
+</div>
 </div>
 </template>
 <script>
+// const axios = require("axios");
 export default {
-    setup() {
-
+  
+    data() {
+return {
+  categoryName: "",
+  categoryDescription: "",
+  imageUrl: "",
+}
+    },
+    methods: {
+      addCategory() {
+        console.log(this.categoryName, this.categoryDescription, this.imageUrl);
+      }
     }
 }
 </script>
