5bebadb11e90ba619b2a58bc9bf14f42794cce48
Fificode <lamidifirdaws@gmail.com>
2023-11-15 21:25:42 +0100

    installed firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAveehadKxUO7kADtgn82qiqiPVOtYBOaA",
+  authDomain: "vue-ecommerce-project-62f3d.firebaseapp.com",
+  projectId: "vue-ecommerce-project-62f3d",
+  storageBucket: "vue-ecommerce-project-62f3d.appspot.com",
+  messagingSenderId: "446480111080",
+  appId: "1:446480111080:web:2bd27bc4ae04b76757405d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
